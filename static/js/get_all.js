c87da6d4d83bbfcb1ab0032e9bc2b39c8540d273
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2023-11-15 13:08:56 -0600

    Update get_all.js

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,6 +1,6 @@
 function getAll(){
-    const URL = "http://127.0.0.1:8000/contactos";
-    //const URL = "ttps://shm-backend-105ae4e301e9.herokuapp.com/contactos";
+    const URL = "https://miapi-1416df5a6c82.herokuapp.com/contactos";
+    //const URL = "https://miapi-1416df5a6c82.herokuapp.com/contactos";
     var request = new XMLHttpRequest;
     request.open('GET',URL);
     request.send();
